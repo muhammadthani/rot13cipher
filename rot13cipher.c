aa893fbe09f8e885bd5dfed083fc4dd38528138a
muhammadthani <thaniabdulkadir@gmail.com>
2022-11-01 02:07:50 -0700

    rot13cipher.c

---
+++ rot13cipher.c
@@ -0,0 +1,45 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+char *rot13(char *p)
+{
+    int array1[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+    int array2[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    int i;
+    int a = 0;
+    while (*(p +a) != '\0')
+    {
+        for (i = 0; i <= 26; i++)
+        {
+            if (*(p +a) == array1[i])
+            *(p +a) = array2[i];
+            else if (*(p +a) == array2[i])
+            *(p +a) = array1[i];
+        }
+        a++;
+    }
+    return (p);
+    
+}
+
+int main(void)
+{
+    char s[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13) is a simple letter substitution cipher.\n";
+    char *p;
+
+    p = rot13(s);
+    printf("%s", p);
+    printf("------------------------------------\n");
+    printf("%s", s);
+    printf("------------------------------------\n");
+    p = rot13(s);
+    printf("%s", p);
+    printf("------------------------------------\n");
+    printf("%s", s);
+    printf("------------------------------------\n");
+    p = rot13(s);
+    printf("%s", p);
+    printf("------------------------------------\n");
+    printf("%s", s);
+    return (0);
+}
